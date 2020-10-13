911ca417541261cca1c1dfba8875f4dd3dc31763
Hemalatha <hemalathavinai@gmail.com>
2020-10-13 17:28:54 +0530

    Sample included

---
+++ GettingStarted/GettingStarted/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GettingStarted
+{
+    public class ViewModel
+    {
+        private bool isChecked = true;
+
+        public Command TransactionCommmand { get; set; }
+        public bool IsChecked
+        {
+            get { return isChecked; }
+            set { isChecked = value; }
+        }
+
+        public ViewModel()
+        {
+            TransactionCommmand = new Command(DoTransaction);
+        }
+        
+        private void DoTransaction()
+        {
+            App.Current.MainPage.DisplayAlert("Hello","Transaction is Initiated ","Ok");
+        }
+    }
+}
